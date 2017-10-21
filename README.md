261d5674a73bc4d7ded9b7fe952f7f6e042efa71
Moritz Heiber <mheiber@thoughtworks.com>
2017-10-21 19:18:07 +0200

    Initial checkin

---
+++ README.md
@@ -0,0 +1,3 @@
+# Alpine base image with envconsul/consul-template
+
+A docker base image which contains [envconsul](https://github.com/hashicorp/envconsul) and [consul\_template](https://github.com/hashicorp/consul-template).
---
+++ spec/alpine_base_image/alpine_base_image_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+# Some helpers for rspec
+module Helpers
+  def os_version
+    command('cat /etc/os-release').stdout
+  end
+end
+
+RSpec.configure do |c|
+  c.include Helpers
+  c.extend Helpers
+end
+
+# rubocop:disable Metrics/BlockLength
+describe 'Alpine base image Docker container', :extend_helpers do
+  set :os, family: :alpine
+  set :backend, :docker
+  set :docker_image, 'alpine-base'
+
+  describe file('/usr/bin/consul-template') do
+    it { should be_file }
+    it { should be_mode 755 }
+    it { should be_owned_by 'root' }
+  end
+
+  describe file('/usr/bin/envconsul') do
+    it { should be_file }
+    it { should be_mode 755 }
+    it { should be_owned_by 'root' }
+  end
+
+  %w[/tmp/envconsul.zip /tmp/consul-template.zip].each do |zip|
+    describe file(zip) do
+      it { should_not exist }
+    end
+  end
+
+  describe 'the operating system' do
+    it 'is alpine' do
+      expect(os_version).to include('alpine')
+    end
+  end
+
+  describe package('curl') do
+    it { should_not be_installed }
+  end
+
+  describe command('consul-template -v') do
+    its(:stderr) { should match(/v0\.19\.3/) }
+  end
+
+  describe command('envconsul -v') do
+    its(:stderr) { should match(/v0\.7\.2/) }
+  end
+end
