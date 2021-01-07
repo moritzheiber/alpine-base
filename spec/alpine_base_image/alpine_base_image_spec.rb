7ffc4d804b6558cdc6000429cb1d99c7b3025a6b
Moritz Heiber <hello@heiber.im>
2021-01-07 09:04:44 +0100

    Updated Alpine, contained software and tests, updated Bundler, added Bundler config, updated gems

--- spec/alpine_base_image/alpine_base_image_spec.rb
+++ spec/alpine_base_image/alpine_base_image_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 # Some helpers for rspec
@@ -24,15 +26,15 @@ describe 'Alpine base image Docker container', :extend_helpers do
     /usr/bin/gomplate
   ].each do |f|
     describe file(f) do
-      it { should be_file }
-      it { should be_mode 755 }
-      it { should be_owned_by 'root' }
+      it { is_expected.to be_file }
+      it { is_expected.to be_mode 755 }
+      it { is_expected.to be_owned_by 'root' }
     end
   end
 
   %w[/tmp/envconsul.zip /tmp/consul-template.zip].each do |zip|
     describe file(zip) do
-      it { should_not exist }
+      it { is_expected.not_to exist }
     end
   end
 
@@ -43,19 +45,19 @@ describe 'Alpine base image Docker container', :extend_helpers do
   end
 
   describe package('curl') do
-    it { should_not be_installed }
+    it { is_expected.not_to be_installed }
   end
 
   describe command('consul-template -v') do
-    its(:stderr) { should match(/v0\.19\.5/) }
+    its(:stderr) { is_expected.to match(/v0\.25\.1/) }
   end
 
   describe command('envconsul -v') do
-    its(:stderr) { should match(/v0\.7\.3/) }
+    its(:stderr) { is_expected.to match(/v0\.11\.0/) }
   end
 
   describe command('gomplate -v') do
-    its(:stdout) { should match(/3\.4\.1/) }
+    its(:stdout) { is_expected.to match(/3\.8\.0/) }
   end
 end
 # rubocop:enable Metrics/BlockLength,RSpec/DescribeClass
